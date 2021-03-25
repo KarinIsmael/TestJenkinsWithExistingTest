cf6f4ac51eb00b83c8127a3d2346f30b3b9471dd
Karin Ismael <karin.ismael@hotmail.com>
2021-03-25 23:49:33 +0100

    Uppgift 2 - all tests with Mockito

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -2,7 +2,12 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class EmployeeManagerTest {
 
@@ -20,9 +25,41 @@ class EmployeeManagerTest {
 
         EmployeeRepository employeeRepository = new TestEmployeeRepository();
         BankService bankService = new TestBankService();
+
+
+
         EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
 
         assertFalse(employeeManager.payEmployees()==2);
+
+    }
+
+    @Test
+    void numberOfEmployeesPaidWithMockito(){
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        BankService bankService = mock(BankService.class);
+
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee("2",25000),
+                new Employee("3", 37000), new Employee("3",24000)));
+
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+        assertEquals(3, employeeManager.payEmployees());
+    }
+
+    @Test
+    void noEmployeesThrowsExceptionWithMockito(){
+
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        BankService bankService = mock(BankService.class);
+
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee("2",25000),
+                new Employee("3", 37000), new Employee("3",24000)));
+
+
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+        assertFalse(employeeManager.payEmployees()==1);
 
     }
 
