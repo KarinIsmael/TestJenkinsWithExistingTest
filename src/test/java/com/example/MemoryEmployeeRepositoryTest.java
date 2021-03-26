fe6d4d2378135796534a3b373a442ebd2b37ac3b
Karin Ismael <karin.ismael@hotmail.com>
2021-03-26 19:00:13 +0100

    Uppgift 3 - In memory repo test 2 (add new to existing list)

--- src/test/java/com/example/MemoryEmployeeRepositoryTest.java
+++ src/test/java/com/example/MemoryEmployeeRepositoryTest.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MemoryEmployeeRepositoryTest {
 
@@ -15,4 +18,19 @@ class MemoryEmployeeRepositoryTest {
         assertThat(memoryEmployeeRepository.findAll()).contains(actual);
 
     }
+
+    @Test
+    void addNewToList(){
+
+        List<Employee> employees = List.of(new Employee("1",25000),
+                new Employee("2", 37000), new Employee("3",24000));
+
+        MemoryEmployeeRepository memoryEmployeeRepository = new MemoryEmployeeRepository(employees);
+
+        memoryEmployeeRepository.save(new Employee("4",30000));
+
+        assertEquals(4,memoryEmployeeRepository.findAll().size());
+
+    }
+
 }
