e1d8b5348f3ab7498e57cae4ffc035f4b8d4cc23
Karin Ismael <karin.ismael@hotmail.com>
2021-03-27 20:13:27 +0100

    Uppgift 3 - changed test and added test for replacing elements in ArrayList. (Removed unused constructors)

--- src/main/java/com/example/MemoryEmployeeRepository.java
+++ src/main/java/com/example/MemoryEmployeeRepository.java
@@ -5,13 +5,12 @@ import java.util.List;
 
 public class MemoryEmployeeRepository implements EmployeeRepository{
 
-    List <Employee> employees = new ArrayList<>();
+    public static List <Employee> employees = new ArrayList<>();
 
-    public MemoryEmployeeRepository() {
-    }
-
-    public MemoryEmployeeRepository(List<Employee> employees) {
-        this.employees.addAll(employees);
+    static {
+        employees.add(new Employee("1",23000));
+        employees.add(new Employee("2",45000));
+        employees.add(new Employee("3",24000));
     }
 
     @Override
--- src/test/java/com/example/MemoryEmployeeRepositoryTest.java
+++ src/test/java/com/example/MemoryEmployeeRepositoryTest.java
@@ -2,8 +2,6 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,7 +10,7 @@ class MemoryEmployeeRepositoryTest {
     @Test
     void saveToList() {
 
-        MemoryEmployeeRepository memoryEmployeeRepository = new MemoryEmployeeRepository();
+       MemoryEmployeeRepository memoryEmployeeRepository = new MemoryEmployeeRepository();
 
         var actual = memoryEmployeeRepository.save(new Employee("1", 20000));
         assertThat(memoryEmployeeRepository.findAll()).contains(actual);
@@ -22,15 +20,23 @@ class MemoryEmployeeRepositoryTest {
     @Test
     void addNewToList(){
 
-        List<Employee> employees = List.of(new Employee("1",25000),
-                new Employee("2", 37000), new Employee("3",24000));
 
-        MemoryEmployeeRepository memoryEmployeeRepository = new MemoryEmployeeRepository(employees);
+        MemoryEmployeeRepository memoryEmployeeRepository = new MemoryEmployeeRepository();
 
         memoryEmployeeRepository.save(new Employee("4",30000));
 
         assertEquals(4,memoryEmployeeRepository.findAll().size());
 
+    }
+
+    @Test
+    void addNewWithSameIdReplacesExistingEmployee(){
+
+        MemoryEmployeeRepository memoryEmployeeRepository = new MemoryEmployeeRepository();
+
+        var b = memoryEmployeeRepository.save(new Employee ("2",5000));
+
+        assertEquals(5000, b.getSalary());
     }
 
 }
